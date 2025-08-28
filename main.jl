2e606809a0766afedbd47bf0a040ac4d7e072dbf
zhaose233 <zhaose233@outlook.com>
2025-08-28 13:35:16 +0800

    slow sim

--- main.jl
+++ main.jl
@@ -31,21 +31,29 @@ function run_with_T(temp)
   end
 
   ensemble_prob = EnsembleProblem(prob)
-  sim = solve(ensemble_prob,SRA1(), EnsembleThreads(), 
-              trajectories=10, dt=0.1, maxiters=1e9,
+  sim = solve(ensemble_prob, SRA1(), EnsembleThreads(), 
+              trajectories=10, dt=τ_end/10000.0, maxiters=1e9,
               callback=cb,
               save_everystep=false, 
               save_start=false)
 
   ts = map(x -> (x.t[end] / normal_j.ω_p), sim.u)
   Is = map(x -> ustrip(u"μA", I_drive(x)), ts)
-
+  println(Is)
   return Is
 end
 
-Iss = map(run_with_T, [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0] .* 100.0u"mK")
-println(map(mean, Iss))
-println(map(std, Iss))
+Ts = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0] .* 100.0u"mK"
+Iss = map(run_with_T, Ts)
+Isw = map(mean, Iss)
+Isw_std = map(std, Iss)
+
+open("out.csv", "w") do f
+  println(f, "T, Isw, Isw_std")
+  for i in eachindex(Ts)
+    println(f, join([ustrip(u"K",Ts[i]), Isw[i], Isw_std[i]], ", "))
+  end
+end
 
 return Iss
 
